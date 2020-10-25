3395fca42334777a6d055293f76833df38904d08
smx <srxboys@126.com>
2020-10-25 11:12:52 +0800

    update: formatting code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 /**
- * Animate.CSS 动画
+ * Animate.CSS
  * https://github.com/animate-css/animate.css
  */
 const AnimateCssMixin = {
   methods: {
     /**
+     * Monitor the animation finish.
      * 动画对象 添加 监听 动画播放完毕
-     * @param {object} target 动画对象
-     * @param {[string]} defaultClass 默认class
+     * @param {Object} target animation object(动画对象)
      */
     animateAddEndListener(target) {
       target.addEventListener('animationend', () => {
@@ -29,8 +29,9 @@ const AnimateCssMixin = {
     },
 
     /**
+     * Remove monitor animation, play finished
      * 移除监听
-     * @param {object} target 动画对象
+     * @param {Object} target animation object(动画对象)
      */
     animateRemoveAllListener(targets) {
       if (!targets || !Array.isArray(targets)) {
@@ -44,13 +45,16 @@ const AnimateCssMixin = {
       target.removeEventListener('animationend')
     },
 
+    /**
+     * loop get DOM.node
+     * @param {Object} target animation object(动画对象)
+     * @param {Function} callback
+     */
     animateChildNodes(target, callback) {
       const elChilds = target.childNodes
-      // console.log('elChilds=>', elChilds)
       if (elChilds && elChilds.length) {
         for (let i = 0; i < elChilds.length; i++) {
           const element = elChilds[i]
-          // console.log('animateChildNodes element=>', element)
           if (element && element.nodeType === 1) {
             if (callback && typeof callback === 'function') {
               callback(element)
@@ -59,10 +63,28 @@ const AnimateCssMixin = {
         }
       }
     },
+    
+    /**
+     * loop get animate object
+     * @param {Object} target animation object(动画对象)
+     * @param {Array} array
+     */
+    animateForElement(target, array) {
+      this.animateChildNodes(target, e => {
+        const animation = e.getAttribute('data-animation')
+        if (animation && typeof animation === 'string') {
+          array.push(e)
+        }
+        if (e.nodeType === 1 && e.childElementCount > 0) {
+          this.animateForElement(e, array)
+        }
+      })
+    },
 
     /**
-     * 获取对象，id 、 class
-     * @param {string} target 字符串
+     * get animation object
+     * 获取对象(id 、 class、ref)
+     * @param {Any<string/Object>} target animation object(动画对象)
      */
     animateGetTarget(target) {
       if (!target) {
@@ -93,37 +115,21 @@ const AnimateCssMixin = {
     },
 
     /**
-     * 播放动画
-     * @param {string} target 父级对象
-     * @param {Array<object>} items 子级对象数组 - 将要播放对象的
+     * play animation
+     * @param {Array<String>} target animation Array(动画对象数组)
      */
     animatePlayground(targets = []) {
-      // console.log('targets=>', targets)
       for (let i = 0; i < targets.length; i++) {
         const itemTarget = targets[i]
-        // console.log('itemTarget=>', itemTarget)
         const itemTargetEl = this.animateGetTarget(itemTarget)
         if (itemTargetEl && itemTargetEl.nodeType === 1) {
-          // class 还原
           const animation = `animate__${itemTargetEl.getAttribute('data-animation')}`
           if (animation && animation !== 'animate__null') {
             this.animateAddEndListener(itemTargetEl)
-            // 添加 动画 class
             itemTargetEl.classList.add('animate__animated', animation)
           }
         }
       }
-    },
-    animateForElement(root, array) {
-      this.animateChildNodes(root, e => {
-        const animation = e.getAttribute('data-animation')
-        if (animation && typeof animation === 'string') {
-          array.push(e)
-        }
-        if (e.nodeType === 1 && e.childElementCount > 0) {
-          this.animateForElement(e, array)
-        }
-      })
     },
   }
 }
