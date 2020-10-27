b072f56a1532f7060dd88ff2cac22acb2fac5d6b
srxboys <srxboys@126.com>
2020-10-27 15:24:14 +0800

    1.0.2 [add] favicon.ico

--- page.js
+++ page.js
@@ -5,12 +5,13 @@ const { removeSync } = require('fs-extra')
 // console.log('process.argv=>', process.argv)
 const getArguments = process.argv.splice(2) || []
 const arg = String(getArguments)
-
+let hrefFavicon = '<link rel="icon" href="/vue-animatecss-mixin/favicon.ico">'
 let hrefCss = '<link rel="stylesheet" type="text/css" href="/vue-animatecss-mixin/main.css" />'
 let hrefVue = '<div><a href="/vue-animatecss-mixin/vue" rel="noopener noopener">Vue demo</a></div>'
 let hrefNuxt = '<div><a href="/vue-animatecss-mixin/nuxt" rel="noopener noopener">Nuxt demo</a></div>'
 
 if (arg === 'dev') {
+  hrefFavicon = '<link rel="icon" href="/favicon.ico">'
   hrefCss = '<link rel="stylesheet" type="text/css" href="/main.css" />'
   hrefVue = '<div><a href="/vue" rel="noopener noopener">Vue demo</a></div>'
   hrefNuxt = '<div><a href="/nuxt" rel="noopener noopener">Nuxt demo</a></div>'
@@ -21,8 +22,10 @@ const docsHtml = join(__dirname, '/docs/index.html')
 
 removeSync(docsHtml)
 
+
 const pageCode = fs.readFileSync(tempHtml, 'utf8')
-let lastCode = pageCode.replace(/tempCssCode/gm, hrefCss)
+lastCode = pageCode.replace(/tempFavicon/gm, hrefFavicon)
+lastCode = lastCode.replace(/tempCssCode/gm, hrefCss)
 lastCode = lastCode.replace(/tempVueCode/gm, hrefVue)
 lastCode = lastCode.replace(/tempNuxtCode/gm, hrefNuxt)
 
